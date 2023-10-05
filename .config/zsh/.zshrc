a20c13af35b5c3f50f504b26eb303e60112936a2
Srikanth Bandaru <srikanth.bandaru@bayer.com>
2023-10-05 16:05:54 -0500

    lazyload nvm & pyenv

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -1,16 +1,6 @@
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(git)
 
-# # nvm and node.js
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# # pyenv and python
-# export PYENV_ROOT="$HOME/.pyenv"
-# command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-# eval "$(pyenv init -)"
-
 source ~/.config/zsh/functions.sh
 
 # zsh-autosuggestions
--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -1,31 +1,45 @@
-function link_dotfiles() {
-    local current_dir="$PWD"
-    
-    # Linking directories in ~/.config
-    echo "Linking directories to ~/.config..."
-    cd ~/projects/dotfiles/.config
-    for dir in *; do
-        # Check if symlink or directory doesn't already exist
-        if [ ! -e ~/.config/"$dir" ]; then  
-            ln -s "$PWD/$dir" ~/.config/
-            echo "Linked $dir to ~/.config/"
-        else
-            echo "~/.config/$dir already exists. Skipping."
-        fi
-    done
+# nvm, node, npm, pnpm
+lazy_load_nvm() {
+    unset -f npm node nvm
+    export NVM_DIR=~/.nvm
+    [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+}
 
-    # Linking individual dotfiles to ~
-    echo "Linking dotfiles to home directory..."
-    for file in ~/projects/dotfiles/.*; do
-        base_name=$(basename "$file")
-        # Check if it's a file and doesn't already exist in ~
-        if [ -f "$file" ] && [ ! -e ~/"$base_name" ]; then  
-            ln -s "$file" ~/
-            echo "Linked $base_name to ~/"
-        elif [ -e ~/"$base_name" ]; then
-            echo "~/$base_name already exists. Skipping."
-        fi
-    done
-    
-    cd "$current_dir"
+npm() {
+    lazy_load_nvm
+    npm $@
+}
+
+pnpm() {
+    lazy_load_nvm
+    pnpm $@
+}
+
+node() {
+    lazy_load_nvm
+    node $@
+}
+
+nvm() {
+    lazy_load_nvm
+    nvm $@
+}
+
+# pyenv, python
+layz_load_pyenv() {
+    unset -f pyenv python
+    export PYENV_ROOT="$HOME/.pyenv"
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+}
+
+pyenv() {
+    layz_load_pyenv
+    pyenv $@
+}
+
+python() {
+    layz_load_pyenv
+    python $@
 }
